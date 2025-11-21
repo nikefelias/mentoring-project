83ceb934f7c8a5a9580ee39b9d296eb3faf04688
«nikefelias» <felias@bk.ru>
2025-11-21 17:35:51 +0100

    add header component

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import logo from '../assets/icons/logo.svg'; // путь к вашему логотипу
+import React from "react";
+import logo from "../assets/icons/logo.svg";
 
 function Header() {
-return ( <header> <div className="logo"> <div className="logo-image"> <img src={logo} alt="Logo" /> </div> <h4>Czech Myst</h4> </div>
-
-```
-  <div className="hamburger-menu"></div>
-</header>
-```
-
-);
+  return (
+    <header>
+      <div className="logo">
+        <div className="logo-image">
+          <img src={logo} alt="Logo" />
+        </div>
+        <h4>Czech Myst</h4>
+      </div>
+      <div className="hamburger-menu"></div>
+    </header>
+  );
 }
 
 export default Header;
