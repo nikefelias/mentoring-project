032a8397fe84bb9c800af0bd25ba69f1d29b80ad
«nikefelias» <felias@bk.ru>
2025-11-21 17:34:02 +0100

    add icons

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../assets/icons/logo.svg'; // путь к вашему логотипу
+
+function Header() {
+return ( <header> <div className="logo"> <div className="logo-image"> <img src={logo} alt="Logo" /> </div> <h4>Czech Myst</h4> </div>
+
+```
+  <div className="hamburger-menu"></div>
+</header>
+```
+
+);
+}
+
+export default Header;
