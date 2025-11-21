77c5e9721803ffa7cac3b53a0e8c55641becf8c5
«nikefelias» <felias@bk.ru>
2025-11-21 17:46:11 +0100

    add header/footer to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
 import './App.css'
+import 'index.css'
+import  Header from '../assets/components/Header.jsx';
+import Footer from '../assets/components/Footer.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-  )
+    <div className="App">
+      <Header />
+      <main>
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
+
 
 export default App
